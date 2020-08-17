1460368e825f15e76128722e2621c540207d1d4f
stephanie rousset <strabon@club-internet.fr>
2020-08-17 17:54:31 +0200

    finish seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,33 @@
-user = User.create(first_name: "Julie", last_name: "Grison", email: "jgrison@gmail.com", password: "coucou123")
+
+puts "Delete buoy"
+Buoy.destroy_all
+
+puts "Delete User"
+User.destroy_all
 
 
-first = Buoy.create!(name: "Nestor", description: "Nestor the crocodile", price: 5, user: user)
-first.image.attach(io: File.open('app/assets/images/crocodile.jpg'), filename: 'crocodile.jpg')
-second = Buoy.create!(name: "Fliper", description: "Fliper the dolphin", price: 3, user: user, photo: 'dolphin.jpg')
-second.image.attach(io: File.open('app/assets/images/dolphin.jpg'), filename: 'dolphin.jpg')
-third = Buoy.create!(name: "Willy", description: "Willy the orca", price: 4, user: user, photo: 'orca_.jpg')
-third.image.attach(io: File.open('app/assets/images/orca_.jpg'), filename: 'orca_.jpg')
-fourth = Buoy.create!(name: "Rainbow", description: "Rainbow the licorn", price: 6, user: user, photo: 'licorne.jpg')
-fourth.image.attach(io: File.open('app/assets/images/licorne.jpg'), filename: 'licorne.jpg')
+puts "creating users"
+
+user = User.create(first_name: "Julie", last_name: "Grison", email: "jgrisond@gmail.com", password: "coucou123")
+
+
+first = Buoy.new(name: "Nestor", description: "Nestor the crocodile", price: 5)
+first.user = user
+
+first.photo.attach(io: File.open('app/assets/images/crocodile.jpg'), filename: 'crocodile.jpg')
+first.save!
+
+second = Buoy.new(name: "Fliper", description: "Fliper the dolphin", price: 3)
+second.user = user
+second.photo.attach(io: File.open('app/assets/images/dolphin.jpg'), filename: 'dolphin.jpg')
+second.save!
+
+third = Buoy.new(name: "Willy", description: "Willy the orca", price: 4)
+third.user = user
+third.photo.attach(io: File.open('app/assets/images/orca_.jpg'), filename: 'orca_.jpg')
+third.save!
+
+fourth = Buoy.new(name: "Rainbow", description: "Rainbow the licorn", price: 6)
+fourth.user = user
+fourth.photo.attach(io: File.open('app/assets/images/licorne.jpg'), filename: 'licorne.jpg')
+fourth.save!
