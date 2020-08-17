98510811862d8d4d9b1aab5fb31562c097fa796b
julie collazos <juliecollazos@hotmail.com>
2020-08-17 15:18:54 +0200

    forms views done - new create edit update

--- app/controllers/buoys_controller.rb
+++ app/controllers/buoys_controller.rb
@@ -1,2 +1,24 @@
 class BuoysController < ApplicationController
+  def new
+    @buoy = Buoy.new
+  end
+
+  def create
+    @buoy = Buoy.new(buoy_params)
+  end
+
+  def edit
+    @buoy = Buoy.find(params[:id])
+  end
+
+  def update
+    @buoy = Buoy.find(params[:id])
+    @buoy.update(buoy_params)
+  end
+
+  private
+
+  def buoy_params
+    params.require(:buoy).permit(:name, :description, :price, :photo)
+  end
 end
