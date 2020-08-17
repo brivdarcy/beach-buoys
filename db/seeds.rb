fb8353ef440a137eed31eeef8acf25d560a0a31d
julie collazos <juliecollazos@hotmail.com>
2020-08-17 17:27:23 +0200

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
+puts "Cleaning database..."
+User.destroy_all
+Buoy.destroy_all
+puts "Database cleared!"
+
 user = User.create(first_name: "Julie", last_name: "Grison", email: "jgrison@gmail.com", password: "coucou123")
 
-
-Buoy.create!(name: "Nestor", description: "Nestor the crocodile", price: 5, user: user, photo: 'crocodile.jpg')
-Buoy.create!(name: "Fliper", description: "Fliper the dolphin", price: 3, user: user, photo: 'dolphin.jpg')
-Buoy.create!(name: "Willy", description: "Willy the orca", price: 4, user: user, photo: 'orca_.jpg')
-Buoy.create!(name: "Rainbow", description: "Rainbow the licorn", price: 6, user: user, photo: 'licorne.jpg')
+Buoy.create!(name: "Nestor", description: "Nestor the crocodile", price: 5, user: user)
+Buoy.create!(name: "Fliper", description: "Fliper the dolphin", price: 3, user: User)
+Buoy.create!(name: "Willy", description: "Willy the orca", price: 4, user: user)
+Buoy.create!(name: "Rainbow", description: "Rainbow the licorn", price: 6, user: user)
+puts "Done!"
