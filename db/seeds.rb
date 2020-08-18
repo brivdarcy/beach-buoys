6d60910b35270692cabf175fc2439466447e75a0
julie collazos <juliecollazos@hotmail.com>
2020-08-18 13:52:47 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,23 +13,23 @@ user = User.create(first_name: "Julie", last_name: "Grison", email: "jgrisond@gm
 
 puts "Creating buoys"
 
-first = Buoy.new(name: "Nestor", description: "Nestor the crocodile", price: 5, address: "16 villa Gaudelet, 75011 Paris", person_number: 1)
+first = Buoy.new(name: "Nestor", description: "Nestor the crocodile", price: 5, address: "16 villa Gaudelet 75011 Paris", person_number: 1)
 first.user = user
 
-first.photo.attach(io: File.open('app/assets/images/crocodile.jpg'), filename: 'crocodile.jpg', address: "Le Perchoir, 75011 Paris", person_number: 1)
+first.photo.attach(io: File.open('app/assets/images/crocodile.jpg'), filename: 'crocodile.jpg', address: "Le Perchoir 75011 Paris", person_number: 1)
 first.save!
 
-second = Buoy.new(name: "Fliper", description: "Fliper the dolphin", price: 3,, address: "16 rue Cambronne, 75015 Paris", person_number: 2)
+second = Buoy.new(name: "Fliper", description: "Fliper the dolphin", price: 3,, address: "16 rue Cambronne 75015 Paris", person_number: 2)
 second.user = user
 second.photo.attach(io: File.open('app/assets/images/dolphin.jpg'), filename: 'dolphin.jpg')
 second.save!
 
 third = Buoy.new(name: "Willy", description: "Willy the orca", price: 4)
 third.user = user
-third.photo.attach(io: File.open('app/assets/images/orca.jpg'), filename: 'orca.jpg', address: "87 rue Oberkampf, 75011 Paris", person_number: 1)
+third.photo.attach(io: File.open('app/assets/images/orca.jpg'), filename: 'orca.jpg', address: "87 rue Oberkampf 75011 Paris", person_number: 1)
 third.save!
 
-fourth = Buoy.new(name: "Rainbow", description: "Rainbow the licorn", price: 6, address: "Place Stalingrad, 75019 Paris", person_number: 1)
+fourth = Buoy.new(name: "Rainbow", description: "Rainbow the licorn", price: 6, address: "Place Stalingrad 75019 Paris", person_number: 1)
 fourth.user = user
 fourth.photo.attach(io: File.open('app/assets/images/licorne.jpg'), filename: 'licorne.jpg')
 fourth.save!
