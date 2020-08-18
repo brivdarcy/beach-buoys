bdbde3e751f8a6442ae17d5960b6d34f85073d12
stephanie rousset <strabon@club-internet.fr>
2020-08-18 13:53:14 +0200

    add dates columns to booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_092552) do
+ActiveRecord::Schema.define(version: 2020_08_18_114650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2020_08_18_092552) do
     t.bigint "buoy_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["buoy_id"], name: "index_bookings_on_buoy_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
